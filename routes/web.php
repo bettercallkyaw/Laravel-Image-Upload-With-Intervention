b95e4b1385dffe527b98f49b05b1b6dc5da1e12b
bettercallkyaw <bettercallkyaw@outlook.com>
2021-11-12 22:19:45 +0630

    auth

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',([GalleryController::class,'index']))->name('mainhome');
 Route::resource('gallery',GalleryController::class);
 Route::get('/gallery/{id}/download',([GalleryController::class,'download']))->name('download');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
